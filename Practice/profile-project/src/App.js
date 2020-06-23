7dd852c68c94bf590de5525ae26ce330a0d23226
ThaoLa <thao.la@gbst.com>
2020-06-23 18:53:22 +0700

    - create project render profile

---
+++ Practice/profile-project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import About from "./component/aboutpage/About";
+
+function App() {
+  return (
+    <div class="about-me">
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
